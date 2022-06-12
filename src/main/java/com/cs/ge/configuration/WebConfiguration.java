349a636cd4881b0bf960f2109f11b0001ef52e80
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-12 21:28:17 +0200

    Build react app

--- src/main/java/com/cs/ge/configuration/WebConfiguration.java
+++ src/main/java/com/cs/ge/configuration/WebConfiguration.java
@@ -12,6 +12,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 }
