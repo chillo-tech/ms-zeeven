3e88c44ebe51d7b940b31bd09830a0f9ae2b6756
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-12 19:25:58 +0200

    Build react app

--- src/main/java/com/cs/ge/controllers/CompteUtilisateurControlleur.java
+++ src/main/java/com/cs/ge/controllers/CompteUtilisateurControlleur.java
@@ -10,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,8 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.mail.MessagingException;
 import java.io.IOException;
 
-
-@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping(consumes = "application/json", produces = "application/json")
