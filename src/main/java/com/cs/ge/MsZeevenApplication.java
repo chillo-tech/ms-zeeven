51a3658743bfbd834af1c933745244d7dcaae1df
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-15 05:32:13 +0200

    log security

--- src/main/java/com/cs/ge/MsZeevenApplication.java
+++ src/main/java/com/cs/ge/MsZeevenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "https://app.zeeven.chillo.fr")
 @EnableMongock
 @SpringBootApplication
 public class MsZeevenApplication {
