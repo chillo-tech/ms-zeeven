4e1d3fa9c4fd2ba86c7c9e6a33862b8a19684bee
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-24 07:41:09 +0200

    Mis à jour token

--- src/main/java/com/cs/ge/services/ImageService.java
+++ src/main/java/com/cs/ge/services/ImageService.java
@@ -18,19 +18,22 @@ public class ImageService {
 
     private final String imagesFolder;
     private final String imagesHost;
+    private final String imagesRootfolder;
 
     public ImageService(
             EventRepository eventRepository,
             @Value("${resources.images.folder}") String imagesFolder,
-            @Value("${resources.images.host}") String imagesHost
+            @Value("${resources.images.host}") String imagesHost,
+            @Value("${resources.images.root}") String imagesRootfolder
     ) {
         this.imagesHost = imagesHost;
         this.imagesFolder = imagesFolder;
+        this.imagesRootfolder = imagesRootfolder;
     }
 
     public void saveTicketImages(Event event, Guest guest) {
         try {
-            String location = String.format("%s/%s/events/%s/tickets/%s.jpg", System.getProperty("user.home"), this.imagesFolder, event.getPublicId(), guest.getProfile().getPublicId());
+            String location = String.format("%s/%s/events/%s/tickets/%s.jpg", this.imagesRootfolder, this.imagesFolder, event.getPublicId(), guest.getProfile().getPublicId());
             log.info("IMAGE LOCATION " + location);
             byte[] decodedBytes = Base64.getDecoder().decode(guest.getTicket());
             FileUtils.writeByteArrayToFile(new File(location), decodedBytes);
