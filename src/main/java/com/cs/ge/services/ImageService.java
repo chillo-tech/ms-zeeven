fda95123644c0e8f64c34cdfca562569e13a24ad
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-24 07:29:41 +0200

    Mis à jour token

--- src/main/java/com/cs/ge/services/ImageService.java
+++ src/main/java/com/cs/ge/services/ImageService.java
@@ -6,7 +6,6 @@ import com.cs.ge.repositories.EventRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -29,12 +28,10 @@ public class ImageService {
         this.imagesFolder = imagesFolder;
     }
 
-    @Async
     public void saveTicketImages(Event event, Guest guest) {
         try {
             String location = String.format("%s/%s/events/%s/tickets/%s.jpg", System.getProperty("user.home"), this.imagesFolder, event.getPublicId(), guest.getProfile().getPublicId());
             log.info("IMAGE LOCATION " + location);
-
             byte[] decodedBytes = Base64.getDecoder().decode(guest.getTicket());
             FileUtils.writeByteArrayToFile(new File(location), decodedBytes);
         } catch (IOException e) {
