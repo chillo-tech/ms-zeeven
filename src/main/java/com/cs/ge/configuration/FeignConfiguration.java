344efe7b5e5635e4ba55f66d28263ea08c226a48
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-24 07:04:54 +0200

    Mis à jour token

--- src/main/java/com/cs/ge/configuration/FeignConfiguration.java
+++ src/main/java/com/cs/ge/configuration/FeignConfiguration.java
@@ -1,10 +1,12 @@
 package com.cs.ge.configuration;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class FeignConfiguration {
 
@@ -16,11 +18,12 @@ public class FeignConfiguration {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
+        log.debug(this.token);
         return requestTemplate -> {
             requestTemplate.header("Content-Type", "application/json");
             requestTemplate.header("Accept", "application/json");
             requestTemplate.header("Authorization", "Bearer " + this.token);
         };
     }
-    
+
 }
