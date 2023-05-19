29813a19ae6ba51244c292a52fb53a36a0b97532
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-05-19 16:32:07 +0200

    ZEEVEN

--- src/main/java/com/cs/ge/dto/ApplicationNotification.java
+++ src/main/java/com/cs/ge/dto/ApplicationNotification.java
@@ -4,22 +4,37 @@ import com.cs.ge.entites.Guest;
 import com.cs.ge.entites.Profile;
 import com.cs.ge.enums.Channel;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-public record ApplicationNotification(
-        @JsonProperty("application") String application,
-        @JsonProperty("template") String template,
-        @JsonProperty("subject") String subject,
-        @JsonProperty("eventId") String eventId,
-        @JsonProperty("message") String message,
-        @JsonProperty("params") Map<String, String> params,
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationNotification implements Serializable {
+    @JsonProperty("application")
+    String application;
+    @JsonProperty("template")
+    String template;
+    @JsonProperty("subject")
+    String subject;
+    @JsonProperty("eventId")
+    String eventId;
+    @JsonProperty("message")
+    String message;
+    @JsonProperty("params")
+    Map<String, String> params;
 
-        @JsonProperty("channels") List<Channel> channels,
-        @JsonProperty("from") Profile from,
-        @JsonProperty("contacts") List<Guest> contacts
-
-) implements Serializable {
+    @JsonProperty("channels")
+    List<Channel> channels;
+    @JsonProperty("from")
+    Profile from;
+    @JsonProperty("contacts")
+    List<Guest> contacts;
 }
