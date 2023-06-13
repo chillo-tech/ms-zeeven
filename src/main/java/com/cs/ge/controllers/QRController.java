3e3b7d3f3b6ca8d9f1c1a69ca696968bf49da251
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-06-13 12:22:09 +0200

    Statistics

--- src/main/java/com/cs/ge/controllers/QRController.java
+++ src/main/java/com/cs/ge/controllers/QRController.java
@@ -4,6 +4,7 @@ import com.cs.ge.entites.QRCodeEntity;
 import com.cs.ge.services.QRCodeGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
+import java.net.URI;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
@@ -29,10 +32,12 @@ public class QRController {
         return this.qrCodeGeneratorService.generate(qrCodeEntity);
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{publicId}")
-    public String get(@PathVariable final String publicId) throws IOException {
-        return this.qrCodeGeneratorService.content(publicId);
+    public ResponseEntity<Object> get(@PathVariable final String publicId) throws IOException {
+        String url = this.qrCodeGeneratorService.content(publicId);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(url);
+        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).location(URI.create(url)).build();
     }
 
 }
