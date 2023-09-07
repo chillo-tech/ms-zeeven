457710854a6b48c0ea55c036e7e3c7f8d7fca32c
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-09-07 13:32:33 +0200

    Envoi du template

--- src/main/java/com/cs/ge/entites/Invitation.java
+++ src/main/java/com/cs/ge/entites/Invitation.java
@@ -1,6 +1,7 @@
 package com.cs.ge.entites;
 
 import com.cs.ge.enums.Channel;
+import com.google.gson.annotations.Expose;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,8 @@ public class Invitation {
     private String publicId;
     private Channel[] channels;
     private Template template;
+    @Expose
     private Instant send;
+    @Expose
     private Instant active;
 }
