1871892f9e58fdedf67dd2209b2381255943a0d1
Achille <simachille@yahoo.fr>
2024-06-22 12:11:59 +0200

    Envoi de notifications

---
+++ src/main/java/com/cs/ge/feign/FIleHandler.java
@@ -0,0 +1,17 @@
+package com.cs.ge.feign;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient(name = "files handler", url = "${app.images-inner-host}")
+public interface FIleHandler {
+
+    @PostMapping({"/v1/files"})
+    void message(
+            @RequestBody Map<String, Object> params);
+
+}
